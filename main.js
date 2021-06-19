7a75684d4b404170a9f64208fe88f3a3b177b494
MaxL-Aryan <79088145+MaxL-Aryan@users.noreply.github.com>
2021-06-19 18:30:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function show()
+{
+    first_name=document.getElementById("first_nme").value;
+    second_name=document.getElementById("scnd_nme").value;
+    age=document.getElementById("age").value;
+    console.log("Name = "+first_name+" "+second_name);
+    console.log("Age = "+age);
+}
